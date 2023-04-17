22cabac9773fd0ea2a51e2502a34af8ddb2c160e
Mikael Alves <47370510+Mikael-Kalashnikov@users.noreply.github.com>
2023-04-17 11:07:28 -0300

    Add files via upload

--- Cabeçalho.h
+++ Cabeçalho.h
@@ -1,13 +1,13 @@
 #define BIBLIOTECA_H
 
 // Função para imprimir o tabuleiro
-void tabuleiro(char);
+void tabuleiro(char [3][3]);
 
 // Função para verificar se o jogador venceu
-inline bool checarVencedor(char, char);
+bool checarVencedor(char [3][3], char);
 
 // Função para verificar se o jogo está empatado
-inline bool checarEmpate(char);
+bool checarEmpate(char [3][3]);
 
-void resetarTabuleiro(char);
+void resetarTabuleiro(char [3][3]);
 
